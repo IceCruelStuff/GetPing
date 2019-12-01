01748b9d5df8e1f2c0d6918a0cad539b8a02e7f1
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2019-11-30 20:30:11 -0800

    Create GetPing.php

---
+++ src/GetPing/GetPing.php
@@ -0,0 +1,58 @@
+<?php
+namespace GetPing;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandExecutor;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\event\Listener;
+use pocketmine\utils\Binary;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\server\DataPacketReceiveEvent;
+class Main extends PluginBase implements CommandExecutor, Listener {
+	protected $lastPing;
+	public function onEnable() : void{
+		$this->lastPing = [];
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+	public function getPing($n) {
+		if (isset($this->lastPing[strtolower($n)])) {
+			return $this->lastPing[strtolower($n)];
+		}
+		return "N/A";
+	}
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
+		switch($command->getName()) {
+			case "getping":
+				if (!count($args)) {
+					foreach ($this->getServer()->getOnlinePlayers() as $m) {
+						$n = $m->getName();
+						$sender->sendMessage($n.": ".$this->getPing($n));
+					}
+				} else {
+					foreach ($args as $n) {
+						$p = $this->getServer()->getPlayer($n);
+						if ($p == null) {
+							$sender->sendMessage($n.": Not found");
+							continue;
+						}
+						$n = $p->getName();
+						$sender->sendMessage($n.": ".$this->getPing($n));
+					}
+				}
+				return true;
+				break;
+		}
+		return false;
+	}
+	//
+	// Event Handlers
+	//
+	public function getPacket(DataPacketReceiveEvent $e) {
+		if ($e->getPacket()->pid() !== 0x00) return;
+		$this->lastPing[strtolower($e->getPlayer()->getName())]
+			= Binary::readLong($e->getPacket()->buffer)/1000.0;
+	}
+	public function isCancelled(PlayerQuitEvent $e) {
+		unset($this->lastPing[strtolower($e->getPlayer()->getName())]);
+	}
+}
