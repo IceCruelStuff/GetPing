adfc1a9985e1853b5c40199e206a1c874d7a6568
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-05-02 18:16:52 -0700

    Fix some errors

--- src/GetPing/GetPing.php
+++ src/GetPing/GetPing.php
@@ -13,57 +13,57 @@ use pocketmine\event\server\DataPacketReceiveEvent;
 
 class GetPing extends PluginBase implements CommandExecutor, Listener {
 
-    protected $lastPing;
+	protected $lastPing;
 
-    public function onEnable() : void {
-        $this->lastPing = [];
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-    }
+	public function onEnable() : void {
+		$this->lastPing = [];
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
 
-    public function getPing($n) {
-        if (isset($this->lastPing[strtolower($n)])) {
-            return $this->lastPing[strtolower($n)];
-        }
-        return "N/A";
-    }
+	public function getPing($n) {
+		if (isset($this->lastPing[strtolower($n)])) {
+			return $this->lastPing[strtolower($n)];
+		}
+		return "N/A";
+	}
 
-    public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool {
-        switch ($command->getName()) {
-            case "getping":
-                if (!count($args)) {
-                    foreach ($this->getServer()->getOnlinePlayers() as $m) {
-                        $n = $m->getName();
-                        $sender->sendMessage($n.": ".$this->getPing($n));
-                    }
-                } else {
-                    foreach ($args as $n) {
-                        $p = $this->getServer()->getPlayer($n);
-                        if ($p == null) {
-                            $sender->sendMessage($n.": Not found");
-                            continue;
-                        }
-                        $n = $p->getName();
-                        $sender->sendMessage($n.": ".$this->getPing($n));
-                    }
-                }
-                return true;
-                break;
-        }
-        return false;
-    }
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool {
+		switch ($command->getName()) {
+			case "getping":
+				if (!count($args)) {
+					foreach ($this->getServer()->getOnlinePlayers() as $m) {
+						$n = $m->getName();
+						$sender->sendMessage($n.": ".$this->getPing($n));
+					}
+				} else {
+					foreach ($args as $n) {
+						$p = $this->getServer()->getPlayer($n);
+						if ($p == null) {
+							$sender->sendMessage($n.": Not found");
+							continue;
+						}
+						$n = $p->getName();
+						$sender->sendMessage($n.": ".$this->getPing($n));
+					}
+				}
+				return true;
+				break;
+		}
+		return false;
+	}
 
-    //
-    // Event Handlers
-    //
-    public function getPacket(DataPacketReceiveEvent $e) {
-        if ($e->getPacket()->pid() !== 0x00) {
-            return;
-        }
-        $this->lastPing[strtolower($e->getPlayer()->getName())] = Binary::readLong($e->getPacket()->buffer)/1000.0;
-    }
+	//
+	// Event Handlers
+	//
+	public function getPacket(DataPacketReceiveEvent $e) {
+		if ($e->getPacket()->pid() !== 0x00) {
+			return;
+		}
+		$this->lastPing[strtolower($e->getPlayer()->getName())] = Binary::readLong($e->getPacket()->buffer)/1000.0;
+	}
 
-    public function isCancelled(PlayerQuitEvent $e) {
-        unset($this->lastPing[strtolower($e->getPlayer()->getName())]);
-    }
+	public function isCancelled(PlayerQuitEvent $e) {
+		unset($this->lastPing[strtolower($e->getPlayer()->getName())]);
+	}
 
 }
